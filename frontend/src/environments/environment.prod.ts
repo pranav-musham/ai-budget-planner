47faa96de5fabf721c216169603dabf5362570fe
Pranav Sai Musham <pranavmusham56@gmail.com>
2026-03-01 04:28:45 -0500

    updated render url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ai-budget-planner-g71q.onrender.com/api' // Update with your Render backend URL
+  apiUrl: 'https://ai-budget-planner-2ln6.onrender.com' // Update with your Render backend URL
 };
